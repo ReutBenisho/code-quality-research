486a068527c887b71a063047dfe7859170f5d6b7
ReutBenisho <xreutx197@gmail.com>
2026-02-21 15:40:56 +0200

    Adding snippets from Buse_2010

---
+++ dataset/Java/Buse_2010/30_Readability_JavaSnippet_Buse2010.java
@@ -0,0 +1,19 @@
+
+/**
+ * Source: Buse and Weimer (2010), Figure 1
+ * Language: Java
+ * Category: Readability / Metric Testing
+ * Description: A method used to evaluate human readability judgments.
+ */
+public class ReadabilitySample {
+    /**
+     * This is hard to read
+     */
+    public double mnr_2(int a1, int b3, int c3, int f2) {
+        {
+            a1++; b3 = c3 - f2;
+            int r = a1 * 3;
+            return b3++ * r--;
+        }
+    }
+}
