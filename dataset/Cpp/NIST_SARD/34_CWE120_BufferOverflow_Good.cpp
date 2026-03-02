a01e758d9b8262f8fe99004caebe083bcd72675f
ReutBenisho <33193596+ReutBenisho@users.noreply.github.com>
2026-03-02 17:02:20 +0200

    Update 34_CWE120_BufferOverflow_Good.cpp

--- dataset/Cpp/NIST_SARD/34_CWE120_BufferOverflow_Good.cpp
+++ dataset/Cpp/NIST_SARD/34_CWE120_BufferOverflow_Good.cpp
@@ -1,12 +1,11 @@
 
 #include <iostream>
-#include <iomanip> // עבור setw
+#include <iomanip>
 
 #define MAXSIZE 40
 
 void process_input_safe() {
     char buffer[MAXSIZE];
-    // תיקון: הגבלת אורך הקלט לגודל החוצץ (מינוס 1 עבור ה-null terminator)
     std::cin >> std::setw(MAXSIZE) >> buffer; 
     std::cout << "Data received safely: " << buffer << std::endl;
 }
