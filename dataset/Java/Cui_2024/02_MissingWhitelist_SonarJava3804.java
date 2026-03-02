eb30dde42c019d4d6d0f52316e91f54e31539b29
ReutBenisho <33193596+ReutBenisho@users.noreply.github.com>
2026-03-02 17:08:40 +0200

    Update 02_MissingWhitelist_SonarJava3804.java

--- dataset/Java/Cui_2024/02_MissingWhitelist_SonarJava3804.java
+++ dataset/Java/Cui_2024/02_MissingWhitelist_SonarJava3804.java
@@ -1,9 +1,3 @@
-/**
- * Source: Cui et al. 2024, Fig 3(b)
- * Issue: PMD #2976
- * Category: Type Resolution / Multi-threading
- * Ground Truth: False Positive (FP)
- */
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -15,7 +9,6 @@ public class Wrapper {
     class MyThreadSafe { }
 
     class Main {
-        // PMD flags these as unsafe volatile fields because it misses the class annotations
         private volatile MyImmutable x;
         private volatile MyThreadSafe y;
     }
