e8055f664853ab524dd0556b0b6a8ea6c24f2d87
ReutBenisho <33193596+ReutBenisho@users.noreply.github.com>
2026-03-02 17:17:06 +0200

    Update 13_LambdaExpressions_PMD1723.java

--- dataset/Java/Cui_2024/13_LambdaExpressions_PMD1723.java
+++ dataset/Java/Cui_2024/13_LambdaExpressions_PMD1723.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class LambdaSample {
+public class Test {
     Runnable someAction = () -> {
         var foo = new ArrayList<String>(5);
         System.err.println(foo);
