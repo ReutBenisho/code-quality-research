823b412031f0ae18ac4775948c7025ecff790840
ReutBenisho <xreutx197@gmail.com>
2026-08-21 21:24:06 +0300

    Translated snippet 6

---
+++ dataset/06_cpp.cpp
@@ -0,0 +1,10 @@
+class Wrapper {
+public:
+    void foo(bool b) {
+        if (b == true) {
+            // Do something
+        } else if (b == false) {
+            // Do something
+        }
+    }
+};
---
+++ dataset/06_py.py
@@ -0,0 +1,8 @@
+class Wrapper:
+    def foo(self, b):
+        if b is True:
+            # Do something
+            pass
+        elif b is False:
+            # Do something
+            pass
