7b24238e984451bfa78eccdd5828bb0b14474c3a
ReutBenisho <xreutx197@gmail.com>
2026-08-21 14:08:31 +0300

    Removed original  snippet

--- dataset/Java/Cui_2024/01.java
+++
@@ -1,9 +0,0 @@
-public class Outerclass {
-    private int[] arr;
-    public int[] getArr() { return arr; } 
-
-    public static class Innerclass {
-        private int[] arr2;
-        public int[] getArr() { return arr2; } 
-    }
-}
